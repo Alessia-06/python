eef96c5f6be9c7696ff1ccd4bcfbe53d2822675e
Alessia-06 <enjieqiu06@gmail.com>
2024-12-11 15:32:00 +0100

    Add files via upload

--- mlp.py
+++ mlp.py
@@ -1,7 +1,8 @@
+import tensorflow as tf
 import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-import tensorflow as tf
+
 
 def gaussiana(x):
     return np.exp(-x*x)
@@ -27,10 +28,31 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 
-#Creazione del modello
+# Creazione del modello
 gauss_mlp = tf.keras.models.Sequential([
     tf.keras.layers.Dense(10, activation='relu', input_shape=(1,)),  # Strato di input e primo hidden
     tf.keras.layers.Dense(10, activation='relu'),  # Secondo hidden layer
     tf.keras.layers.Dense(1)  # Strato di output
 ])
 
+# Compilazione del modello
+lr = 0.01
+gauss_mlp.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr), loss="mean_squared_error", metrics=['mean_squared_error'])
+
+# Addestramento del modello
+n_epochs = 150
+batch_size = 64
+history = gauss_mlp.fit(x_train_gauss, y_train_gauss, validation_split=0.2, epochs=n_epochs, batch_size=batch_size, verbose=1)
+
+# Valutazione del modello
+test_loss = gauss_mlp.evaluate(x_test_gauss, y_test_gauss, verbose=0)
+print(f"Loss sul set di test: {test_loss}")
+
+# Predizioni sul set di test
+y_pred_gauss = gauss_mlp.predict(x_test_gauss)
+
+# Grafico set di test
+plt.plot(x_gauss, y_gauss, color='gray', label='Gaussiana')
+plt.scatter(x_test_gauss, y_pred_gauss, color = 'red', label='test')
+plt.legend()
+plt.show()
